543a1c7122cd48c96bf40448ed809c3ae1cf28d2
lebashir <bashir.abihabib@gmail.com>
2017-06-30 16:26:28 -0500

    completed user-story-6-seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,40 +7,42 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
-# Create user
+# Create Users
+5.times do
+  User.create!(
+  email:    Faker::Internet.email,
+  password: RandomData.random_sentence
+  )
+end
 
+# Create admin user
+1.times do
+  User.create!(
+  email:     'realtimschmidt@gmail.com',
+  password:  'helloworld',
+  role:       2
+  )
+end
 
-  u =   User.new(
-      email: "jose@bloc.io",
-      password: "password",
-      role: :member
-    )
-  u.skip_confirmation!
-  u.save!
+# Create standard user
+1.times do
+  User.create!(
+  email:    Faker::Internet.email,
+  password: 'helloworld'
+  )
+end
+users = User.all
 
-  tester = User.first
- # Create Wikis
- 50.times do
-   Wiki.create!(
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+# Create Wikis
+50.times do
+  Wiki.create!(
+    user:   users.sample,
+    title:  Faker::Name.title,
+    body:   RandomData.random_paragraph
+  )
+end
+wikis = Wikis.all
 
-   )
- end
- wikis = Wiki.all
-
- 5.times do
-   Wiki.create!(
-   title:  RandomData.random_sentence,
-   body:   RandomData.random_paragraph,
-   private: true,
-   user:  tester
-   )
- end
-
-
-
-
- puts "Seed finished"
- puts "#{Wiki.count} wikis created"
- puts "#{User.count} users created"
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
