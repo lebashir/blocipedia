e2764f5c54e4942eb42e75eb9802d33dba085990
lebashir <bashir.abihabib@gmail.com>
2017-07-06 20:00:55 -0500

    markdown added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def markdown(text)
+    require 'redcarpet'
+    renderer = Redcarpet::Render::HTML.new
+    extensions = { :autolink => true }
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
 end
